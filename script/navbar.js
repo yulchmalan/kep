86048b87f75f2399d15ea046743f8072c95f8e4f
yulch <159718838+yulchmalan@users.noreply.github.com>
2024-12-30 17:37:31 +0200

    Add files via upload

--- script/navbar.js
+++ script/navbar.js
@@ -10,24 +10,7 @@ const elements = document.querySelectorAll(".move-el");
 const screenWidth = window.innerWidth;
 let lastScrollY =  window.scrollY;
 
-burger.addEventListener('click', () => {
-    navList.classList.toggle('nav-active');
-
-    if (navList.classList.contains('nav-active')) {
-        icon.setAttribute('href', 'images/icons.svg#close');
-        topbar.classList.add("topbar-hidden");
-    } else {
-        icon.setAttribute('href', 'images/icons.svg#burger');
-    }
-});
-
-window.addEventListener("scroll", () => {
-    if (lastScrollY < window.scrollY) {
-        topbar.classList.add("topbar-hidden");
-    } else {
-        topbar.classList.remove("topbar-hidden");
-    }
-
+function updateElementTransforms() {
     const isTopbarVisible = !topbar.classList.contains("topbar-hidden");
 
     elements.forEach(element => {
@@ -39,29 +22,55 @@ window.addEventListener("scroll", () => {
             element.style.transform = `translateY(${isTopbarVisible ? "100px" : "40px"})`;
         }
     });
+}
 
+burger.addEventListener('click', () => {
+    navList.classList.toggle('nav-active');
+
+    if (navList.classList.contains('nav-active')) {
+        icon.setAttribute('href', 'images/icons.svg#close');
+        topbar.classList.add("topbar-hidden");
+    } else {
+        icon.setAttribute('href', 'images/icons.svg#burger');
+    }
+    updateElementTransforms();
+});
+
+window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+        topbar.classList.add("topbar-hidden");
+    } else {
+        topbar.classList.remove("topbar-hidden");
+    }
+    updateElementTransforms();
     lastScrollY = window.scrollY;
   });
 
 const dropdowns = document.querySelectorAll('.nav-links li.dropdown');
+const eventType = screenWidth < 1024 ? 'click' : 'mouseover';
 
 if (dropdowns) {
     dropdowns.forEach((item) => {
-        item.addEventListener("click", () => {
+        item.addEventListener(eventType, () => {
             dropdowns.forEach((otherItem) => {
                 if (otherItem !== item) {
                     otherItem.classList.remove("active");
                 }
             });
             item.classList.toggle("active");
+            if (eventType === 'click') {
+                topbar.classList.add("topbar-hidden");
+                updateElementTransforms();
+            }
         });
     });
 }
 
-document.addEventListener("click", (event) => {
+document.addEventListener(eventType, (event) => {
     dropdowns.forEach((item) => {
         if (!item.contains(event.target)) {
             item.classList.remove("active");
+            updateElementTransforms();
         }
     });
 });
