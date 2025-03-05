cb44209b73d355246e8d30b8900e03177f5c749a
yulch <juliamalanjuk@gmail.com>
2025-03-05 16:43:42 +0200

    vrodi pracuje?

--- script/header.js
+++ script/header.js
@@ -41,7 +41,7 @@ const headerContent = `
         <div class="container">
           <a href="index.html" class="logo"
             ><img
-              src="images/logo.png"
+              src="../images/logo.png"
               alt="Логотип Коледжу електронних приладів"
             />
           </a>
@@ -72,8 +72,8 @@ const headerContent = `
                 >Освітня діяльність <i class="fa-solid fa-chevron-down"></i
               ></span>
               <ul class="dropdown-menu">
-                <li><a href="edu_activity_sub_pages/psychologist.html">Методична рада</a></li>
-                <li><a href="edu_activity_sub_pages/activity_clubs.html">Виховна діяльність</a></li>
+                <li><a href="../edu_activity_sub_pages/psychologist.html">Методична рада</a></li>
+                <li><a href="../edu_activity_sub_pages/activity_clubs.html">Виховна діяльність</a></li>
                 <li><a href="#">Виробнича діяльність</a></li>
               </ul>
             </li>
@@ -91,7 +91,7 @@ const headerContent = `
               <span>Викладачу <i class="fa-solid fa-chevron-down"></i></span>
               <ul class="dropdown-menu">
                 <li><a href="#">Атестація педагогічних працівників</a></li>
-                <li><a href="pages/teacher/training.html">Підвищення кваліфікації</a></li>
+                <li><a href="../pages/teacher/training.html">Підвищення кваліфікації</a></li>
                 <li><a href="#">Школа нового педагога</a></li>
                 <li><a href="#">Методичні матеріали</a></li>
               </ul>
